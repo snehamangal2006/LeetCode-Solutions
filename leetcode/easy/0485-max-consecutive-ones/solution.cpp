ff59dc566240ad4f8081b0885febb1eb82ec82f4
snehamangal2006 <snehamangal9446@gmail.com>
2026-08-18 00:39:31 +0530

    ✅ Max Consecutive Ones | Easy | C++ | Runtime: 6 ms (2.84%) | Memory: 50.2 MB | 2026-08-17

---
+++ leetcode/easy/0485-max-consecutive-ones/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int sum=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                sum++;
+            } else sum=0;
+            maxi = max(maxi,sum);
+        } return maxi;
+    }
+};
